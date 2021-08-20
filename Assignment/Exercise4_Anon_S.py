8f7980e6ae679318707407a9e76f8ef7521c62f7
Anon Suphatphon <AnonSuphatphon@outlook.com>
2021-08-20 14:00:36 +0700

    back up

---
+++ Assignment/Exercise4_Anon_S.py
@@ -0,0 +1,13 @@
+# Input
+print("Hello (^_^)\nWhat is your score")
+FE = float(input("Foundation English:"))
+GB = float(input("General Business:"))
+ITCS = float(input("Introduction to Computer Systems:"))
+CP = float(input("Computer Programming:"))
+
+# Output
+print("\n-------------Your Score-------------")
+print("Foundation English : ",FE)
+print("General Business : ",GB)
+print("Introduction to Computer Systems :",ITCS)
+print("Computer Programming :",CP)
---
+++ README.md
@@ -0,0 +1,2 @@
+# CP3-Anon-Suphatphon
+ borntoDev
